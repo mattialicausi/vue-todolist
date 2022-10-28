353f329eabb5c91dece9d264e295b37fffaaa8ff
mattialicausi <licausivitomattia@gmail.com>
2022-10-28 13:29:57 +0200

    preparazione repo

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const {createApp} = Vue;
+
+//creo app
+const app = createApp({
+    data(){
+        return {
+          
+
+        }
+    },
+    methods: {
+   
+     
+    },
+    mounted(){
+       
+
+    }
+});
+app.mount('#app');
